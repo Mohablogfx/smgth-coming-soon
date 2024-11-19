e8abb8e42c8b271cdf8185bfb3afa6514c8c852a
Mohamed YOUSFI <mr.mohamed.yousfi@gmail.com>
2024-11-19 23:00:36 +0100

    Initialisation & Upload the coming soon Files

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+(function ($) {
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+    });
+    
+
+    // ABOUT SLIDER
+    $('body').vegas({
+        slides: [
+            { src: 'images/slide-image01.jpg' },
+            { src: 'images/slide-image02.jpg' },
+            { src: 'images/slide-image03.jpg' },
+            { src: 'images/slide-image04.jpg' },
+            { src: 'images/slide-image05.jpg' },
+            { src: 'images/slide-image06.jpg' },
+            { src: 'images/slide-image07.jpg' },
+            { src: 'images/slide-image08.jpg' }
+        ],
+        timer: false,
+        transition: [ 'zoomOut', ]
+    });
+
+})(jQuery);
